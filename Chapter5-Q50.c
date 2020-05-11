dc30694bd299bffc0c69e4150a35c8a51cd5cbe8
halilgezer <55877853+halilgezer@users.noreply.github.com>
2020-05-12 02:32:32 +0300

    Computer-Assisted Instruction

--- Chapter5-Q50.c
+++ Chapter5-Q50.c
@@ -1,11 +1,11 @@
 /*
 Author is : Ibrahim Halil GEZER
-5.50 (Computer-Assisted Instruction) The use of computers in education is referred to as computer-
+5.50 (Computer-Assisted Instruction)  The use of computers in education is referred to as computer-
 assisted instruction (CAI). Write a program that will help an elementary school student learn
 multiplication. Use the rand function to produce two positive one-digit integers. The program
 should then prompt the user with a question, such as
 How much is 6 times 7?
-The student then inputs the answer. Next, the program checks the student’s answer. If it’s correct,
+The student then inputs the answer. Next, the program checks the studentâ€™s answer. If itâ€™s correct,
 display the message "Very good!" and ask another multiplication question. If the answer is wrong,
 display the message "No. Please try again." and let the student try the same question repeatedly
 until the student finally gets it right. A separate function should be used to generate each new question.
