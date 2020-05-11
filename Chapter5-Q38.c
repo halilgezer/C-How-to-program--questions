7db4bb79079a3cf0dd73f2e63c42560305bc3d3d
halilgezer <55877853+halilgezer@users.noreply.github.com>
2020-05-11 17:13:12 +0300

    Visualizing Recursion

--- Chapter5-Q38.c
+++ Chapter5-Q38.c
@@ -1,10 +1,10 @@
 /*
 Author is : Ibrahim Halil GEZER
-5.38 (Visualizing Recursion) Itís interesting to watch recursion ìin action.î Modify the factorial
+5.38 (Visualizing Recursion) It‚Äôs interesting to watch recursion ‚Äúin action.‚Äù Modify the factorial
 function of Fig. 5.14 to print its local variable and recursive call parameter. For each recursive call,
 display the outputs on a separate line and add a level of indentation. Do your utmost to make the
 outputs clear, interesting and meaningful. Your goal here is to design and implement an output format
-that helps a person understand recursion better. You may want to add such display capabilities
+that helps a person understand recursion better.  You may want to add such display capabilities
 to the many other recursion examples and exercises throughout the text.
 */
 
