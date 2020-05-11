fd068efc6080ce91736f8f3a473ca47e28e6ef3c
halilgezer <55877853+halilgezer@users.noreply.github.com>
2020-05-11 17:05:52 +0300

    Quality points of student average

--- Chapter5-Q30.c
+++ Chapter5-Q30.c
@@ -1,9 +1,9 @@
 /*
 Author is : Ibrahim Halil GEZER 
-5.30 Write a function qualityPoints that inputs a student’s average and returns 4 if a student's
-average is 90–100, 3 if the average is 80–89, 2 if the average is 70–79, 1 if the average is 60–69, and
+5.30 Write a function qualityPoints that inputs a studentâ€™s average and returns 4 if a student's
+average is 90â€“100, 3 if the average is 80â€“89, 2 if the average is 70â€“79, 1 if the average is 60â€“69, and
 0 if the average is lower than 60.
-*/
+*/ 
 
 #include <stdio.h>
 
